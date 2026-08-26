4e80f27278d97cf6a7e0829b18fcb3b2dffc1b27
mpuig-ma <mpuig-ma@student.42barcelona.com>
2026-08-26 12:03:28 +0200

    add standalone script for ci-userdata

--- vm.sh
+++ vm.sh
@@ -55,74 +55,11 @@ vm__cloudinit() {
 	local USER=${2:-"$USER"};
 	local KEY="${SSH_DIR:?}/$(vm__keyname $DOMAIN $USER)";
 
-# user-data.yaml
-cat <<EOF > "${PROJECT_DIR}/user-data.yaml"
-#cloud-config
-users:
-  - name: ${USER}
-    sudo: ALL=(ALL) NOPASSWD:ALL
-    shell: /bin/bash
-    lock_passwd: true
-    ssh_authorized_keys:
-      - $(cat "${KEY}.pub")
-  - name: ${USER}dev
-    sudo: ALL=(ALL) NOPASSWD:ALL
-    shell: /bin/bash
-    lock_passwd: false
-    plain_text_passwd: "root"
-ssh:
-  emit_keys_to_console: false
-apt:
-  conf: |
-    APT::Install-Recommends "false";
-    APT::Install-Suggests "false";
-  sources:
-    hashicorp:
-      keyid: 798A EC65 4E5C 1542 8C8E  42EE AA16 FCBC A621 E701
-      source: 'deb [arch=$(dpkg --print-architecture) signed-by=\$KEY_FILE] https://apt.releases.hashicorp.com \$RELEASE main'
-package_update: true
-package_upgrade: false
-packages:
-- bridge-utils
-- qemu-system-x86
-- qemu-utils
-- libvirt-daemon-system
-- libvirt-clients
-- libvirt-dev
-- nfs-kernel-server
-- build-essential
-- pkgconf
-- vagrant
-allow_public_ssh_keys: true
-disable_root: true
-disable_root_opts: no-port-forwarding,no-agent-forwarding,no-X11-forwarding
-ssh_deletekeys: true
-ssh_quiet_keygen: true
-mounts:
-  - ["shared9p", "/mnt/$PROJECT_NAME", "9p", "trans=virtio,version=9p2000.L,nofail,x-mount.mkdir", "0", "0"]
-bootcmd:
-  - printf "%s\n%s" "[Unit]" "After=cloud-init.target" | sudo systemctl edit sshd.service --stdin
-  - systemctl daemon-reload
-runcmd:
-  - [ groupmod, -g, "$(id --group)", $USER ]
-  - [ usermod, -u, "$(id --user)", $USER ]
-  - chown $USER:$USER /mnt/$PROJECT_NAME
-  - usermod -aG libvirt,kvm ${USER}
-  - vagrant plugin install vagrant-libvirt
-  - virsh net-destroy default
-  - virsh net-undefine default
-  - sed --in-place 's/192\.168/10\.0/g' /usr/share/libvirt/networks/default.xml
-  - virsh net-define /usr/share/libvirt/networks/default.xml
-  - virsh net-start default
-  - needrestart -r a
-final_message: Wubba Lubba dub-dub!
-EOF
-
-# meta-data.yaml
-cat <<EOF > "${PROJECT_DIR}/meta-data.yaml"
-local-hostname: ${VM_NAME}
-EOF
-
+	local CI_USERDATA="${PROJECT_DIR}/user-data.yaml";
+	source "./ci-userdata.sh";
+	if debug; then
+		cloud-init schema --config-file "${CI_USERDATA:?}";
+	fi
 }
 
 vm__hostfwd() {
