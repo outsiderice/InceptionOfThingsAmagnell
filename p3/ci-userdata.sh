ad7eaaa78b31ad175c2b0cd14cb6a3bedd986ec9
outsiderice <arantxamagnelli@gmail.com>
2026-09-01 16:23:07 +0200

    feat: argocd cli provisioned to vm

--- p3/ci-userdata.sh
+++ p3/ci-userdata.sh
@@ -38,5 +38,8 @@ runcmd:
   - install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
   - rm kubectl
   - usermod -aG docker ${USER}
+  - curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+  - install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+  - rm argocd-linux-amd64
 final_message: Wubba Lubba dub-dub!
 EOF
