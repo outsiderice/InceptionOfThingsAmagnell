ae82edff78364d233b277a2ffb5d6c89ab9e7d2f
outsiderice <arantxamagnelli@gmail.com>
2026-09-07 18:00:26 +0200

    feat: added appliaction.yaml which fetches app config from remote repo

--- p3/ci-userdata.sh
+++ p3/ci-userdata.sh
@@ -51,5 +51,6 @@ runcmd:
   - kubectl create namespace argocd
   - kubectl create namespace dev
   - kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#  - kubectl apply -f application.yaml
 final_message: Wubba Lubba dub-dub!
 EOF
